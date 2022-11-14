36c2a7b810f2c8ea0395d135d308d021dd225c7c
perli <1003914407@qq.com>
2022-11-14 15:24:17 +0800

    feat: 实现effect的scheduler

--- src/recativity/effect.ts
+++ src/recativity/effect.ts
@@ -2,13 +2,13 @@
  * @Author: perli 1003914407@qq.com
  * @Date: 2022-11-10 17:23:58
  * @LastEditors: perli 1003914407@qq.com
- * @LastEditTime: 2022-11-11 09:49:59
+ * @LastEditTime: 2022-11-14 15:22:57
  * @FilePath: /min-vue3/src/recativity/effect.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 class ReactiveEffect{
   private _fn: any
-  constructor(fn){
+  constructor(fn,public scheduler){
     this._fn=fn
   }
   run(){
@@ -34,13 +34,17 @@ export function trigger(target,key){
  const depsMap= targetMap.get(target)
  const dep=depsMap.get(key)
  for(const effect of dep){
-  effect.run()
+  if(effect.scheduler){
+    effect.scheduler()
+  }else{
+    effect.run()
+  }
  }
 
 }
 let activeEffect
-export function effect(fn){
-  const _effect = new ReactiveEffect(fn)
+export function effect(fn,option:any={}){
+  const _effect = new ReactiveEffect(fn,option.scheduler)
   _effect.run()
   return _effect.run.bind(_effect)
 }
