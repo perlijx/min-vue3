36972dc10e3b8a94859cbe90f5fb7802760d2732
perli <1003914407@qq.com>
2022-11-14 15:31:58 +0800

    11

--- src/recativity/tests/effect.spec.ts
+++ src/recativity/tests/effect.spec.ts
@@ -2,7 +2,7 @@
  * @Author: perli 1003914407@qq.com
  * @Date: 2022-11-10 17:18:09
  * @LastEditors: perli 1003914407@qq.com
- * @LastEditTime: 2022-11-11 09:48:31
+ * @LastEditTime: 2022-11-14 15:31:53
  * @FilePath: /min-vue3/src/recativity/tests/effect.spec.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -43,6 +43,7 @@ describe("effect",()=>{
     let run:any
     const scheduler = jest.fn(()=>{
       run = runner
+      11
     })
     const obj = reactive({foo:1})
     const runner= effect(()=>{
