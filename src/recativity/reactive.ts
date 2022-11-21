7c4d102f87fb7157cae260d2f3ef1e2812977356
perli <1003914407@qq.com>
2022-11-21 10:17:40 +0800

    fix: 移除多余代码

--- src/recativity/reactive.ts
+++ src/recativity/reactive.ts
@@ -1,11 +1,10 @@
 import { mutableHandler, readOnlyHandler } from './baseHandlers'
-import { track, trigger } from './effect'
 
 /*
  * @Author: perli 1003914407@qq.com
  * @Date: 2022-11-10 16:48:12
  * @LastEditors: perli 1003914407@qq.com
- * @LastEditTime: 2022-11-18 11:42:53
+ * @LastEditTime: 2022-11-21 10:17:22
  * @FilePath: /min-vue3/src/recativity/reactive.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
