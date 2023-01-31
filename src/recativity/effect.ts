566e2474f7254301d9b75eea7958ee4bfe8548e5
perli <1003914407@qq.com>
2023-01-31 14:54:27 +0800

    feat: 优化stop

--- src/recativity/effect.ts
+++ src/recativity/effect.ts
@@ -1,42 +1,53 @@
-import { extend } from './../../shared/index';
+import { extend } from './../../shared/index'
 /*
  * @Author: perli 1003914407@qq.com
  * @Date: 2022-11-10 17:23:58
  * @LastEditors: perli 1003914407@qq.com
- * @LastEditTime: 2022-11-17 17:15:35
+ * @LastEditTime: 2023-01-31 14:52:19
  * @FilePath: /min-vue3/src/recativity/effect.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
+
+let activeEffect
+let shouldTrack
 class ReactiveEffect {
   private _fn: any
   deps = []
-  public schedule:Function|undefined
-  active=true
-  onStop?:()=>void
-  constructor(fn,  scheduler?:Function) {
+  public schedule: Function | undefined
+  active = true
+  onStop?: () => void
+  constructor (fn, scheduler?: Function) {
     this._fn = fn
   }
-  run() {
+  run () {
+    if (!this.active) {
+      return this._fn()
+    }
     activeEffect = this
-    return this._fn()
+    shouldTrack = true
+    const result = this._fn()
+    shouldTrack = false
+    return result
   }
-  stop() {
-    if(this.active){
+  stop () {
+    if (this.active) {
       cleanUpEffect(this)
-      if(this.onStop){
+      if (this.onStop) {
         this.onStop()
       }
     }
-    this.active=false
+    this.active = false
   }
 }
-function cleanUpEffect(effect){
+function cleanUpEffect (effect) {
   effect.deps.forEach((dep: any) => {
     dep.delete(effect)
   })
+  effect.deps.length = 0
 }
 const targetMap = new Map()
-export function track(target, key) {
+export function track (target, key) {
+  if (!isTracking()) return
   let depMap = targetMap.get(target)
   if (!depMap) {
     depMap = new Map()
@@ -47,11 +58,14 @@ export function track(target, key) {
     dep = new Set()
     depMap.set(key, dep)
   }
-  if(!activeEffect) return
+  if (dep.has(activeEffect)) return
   dep.add(activeEffect)
   activeEffect.deps.push(dep)
 }
-export function trigger(target, key) {
+function isTracking () {
+  return shouldTrack && activeEffect !== undefined
+}
+export function trigger (target, key) {
   const depsMap = targetMap.get(target)
   const dep = depsMap.get(key)
   for (const effect of dep) {
@@ -62,16 +76,15 @@ export function trigger(target, key) {
     }
   }
 }
-let activeEffect
-export function effect(fn, option: any = {}) {
+export function effect (fn, option: any = {}) {
   const _effect = new ReactiveEffect(fn, option.scheduler)
-  extend(_effect,option)
+  extend(_effect, option)
   _effect.run()
   const runner: any = _effect.run.bind(_effect)
   runner.effect = _effect
   return runner
 }
 
-export function stop(runner) {
+export function stop (runner) {
   runner.effect.stop()
 }
--- src/recativity/tests/effect.spec.ts
+++ src/recativity/tests/effect.spec.ts
@@ -2,7 +2,7 @@
  * @Author: perli 1003914407@qq.com
  * @Date: 2022-11-10 17:18:09
  * @LastEditors: perli 1003914407@qq.com
- * @LastEditTime: 2022-11-17 17:02:17
+ * @LastEditTime: 2023-01-31 14:53:04
  * @FilePath: /min-vue3/src/recativity/tests/effect.spec.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -71,7 +71,7 @@ describe("effect",()=>{
     obj.prop=2
     expect(dummy).toBe(2)
     stop(runner)
-    obj.prop=3
+    obj.prop++
     expect(dummy).toBe(2)
     runner()
     expect(dummy).toBe(3)
