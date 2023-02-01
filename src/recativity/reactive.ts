4e11a93b2de55f5ab74c3943c164febd064d7147
perli <1003914407@qq.com>
2023-02-01 10:26:42 +0800

    feat: 实现isProxy

--- src/recativity/reactive.ts
+++ src/recativity/reactive.ts
@@ -4,7 +4,7 @@ import { mutableHandler, readOnlyHandler, shallowReadOnlyHandler } from './baseH
  * @Author: perli 1003914407@qq.com
  * @Date: 2022-11-10 16:48:12
  * @LastEditors: perli 1003914407@qq.com
- * @LastEditTime: 2023-02-01 10:16:45
+ * @LastEditTime: 2023-02-01 10:26:11
  * @FilePath: /min-vue3/src/recativity/reactive.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -33,4 +33,7 @@ export function isReactive(value) {
 }
 export function isReadOnly(value) {
   return !! value[ReactiveFlags.IS_READONLY]
+}
+export function isProxy(value) {
+  return isReactive(value) || isReadOnly(value) 
 }
--- src/recativity/tests/reactive.spec.ts
+++ src/recativity/tests/reactive.spec.ts
@@ -2,11 +2,11 @@
  * @Author: perli 1003914407@qq.com
  * @Date: 2022-11-10 16:20:21
  * @LastEditors: perli 1003914407@qq.com
- * @LastEditTime: 2023-01-31 15:01:04
+ * @LastEditTime: 2023-02-01 10:26:26
  * @FilePath: /min-vue3/src/recativity/tests/reactive.spec.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-import { isReactive, reactive } from "../reactive"
+import { isProxy, isReactive, reactive } from "../reactive"
 
 describe("reactive",()=>{
   it("happy path",()=>{
@@ -16,5 +16,10 @@ describe("reactive",()=>{
     expect(isReactive(observed.bar)).toBe(true)
     expect(observed).not.toBe(original)
     expect(observed.foo).toBe(1)
+  })
+  it("isProxy",()=>{
+    const original = {foo:1,bar:{age:1}}
+    const observed=reactive(original)
+    expect(isProxy(observed)).toBe(true)
   })
 })
